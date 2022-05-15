49812d410ece8d9806fdb9c328c1b48986ff728b
amimhayden22 <amim.opreker22@gmail.com>
2022-05-15 13:48:26 +0700

    menambahkan libary laravel ui

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Route::get('/', function () {
 Route::prefix('dashboard')->group(function () {
     Route::resource('users', UserController::class);
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
