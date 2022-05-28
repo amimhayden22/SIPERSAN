fcb766348c70f05d6a095ab0defd94be97152438
amimhayden22 <amim.opreker22@gmail.com>
2022-05-28 20:17:15 +0700

    memperbarui tampilan dan menambahkan menu baru

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RoomController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\DormitoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,13 +27,8 @@ Route::get('/', function () {
 });
 
 Route::prefix('dashboard')->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::resource('/dormitories', DormitoryController::class);
+    Route::resource('/rooms', RoomController::class);
     Route::resource('users', UserController::class);
 });
-
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::resource('/rooms', App\Http\Controllers\RoomController::class);
-
-Route::resource('/dormitories', App\Http\Controllers\DormitoryController::class);
-
