f7b2563624b9e5971a9ebc3e042dc2af312a43c3
amimhayden22 <amim.opreker22@gmail.com>
2022-06-02 10:59:35 +0700

    menambahkan beberapa akun

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,29 +18,103 @@ class UserSeeder extends Seeder
     {
         collect([
             [
+                'id' => 1,
                 'name' => 'Gus Khamim',
                 'email' => 'khamim@gmail.com',
                 'password' => Hash::make('password'),
                 'role' => 'Pengurus Pondok'
             ],
             [
+                'id' => 2,
                 'name' => 'Admin Pengurus Pondok',
                 'email' => 'pengurus_pondok@gmail.com',
                 'password' => Hash::make('passwordAdmin123'),
                 'role' => 'Pengurus Pondok'
             ],
             [
+                'id' => 3,
                 'name' => 'Admin Pengurus Asrama',
                 'email' => 'pengurus_asrama@gmail.com',
                 'password' => Hash::make('passwordAdmin123'),
                 'role' => 'Pengurus Asrama'
             ],
             [
+                'id' => 4,
                 'name' => 'Admin Ketua kamar',
                 'email' => 'ketua_kamar@gmail.com',
                 'password' => Hash::make('passwordAdmin123'),
                 'role' => 'Ketua kamar'
-            ]
+            ],
+            [
+                'id' => 5,
+                'name' => 'Muhammad Yusuf Ridlo',
+                'email' => 'yusufridlo16@gmail.com',
+                'password' => Hash::make('password123!'),
+                'role' => 'Pengurus Pondok'
+            ],
+            [
+                'id' => 6,
+                'name' => 'Rifki Hakim Pradana',
+                'email' => 'rhp@gmail.com',
+                'password' => Hash::make('password123!'),
+                'role' => 'Pengurus Asrama'
+            ],
+            [
+                'id' => 7,
+                'name' => 'Abdul Hamit',
+                'email' => 'ahap@gmail.com',
+                'password' => Hash::make('password123!'),
+                'role' => 'Pengurus Asrama'
+            ],
+            [
+                'id' => 8,
+                'name' => 'Afandy',
+                'email' => 'afap@gmail.com',
+                'password' => Hash::make('password123!'),
+                'role' => 'Pengurus Asrama'
+            ],
+            [
+                'id' => 9,
+                'name' => 'Muhammad Galih Riskiawan',
+                'email' => 'mgr@gmail.com',
+                'password' => Hash::make('password123!'),
+                'role' => 'Ketua Kamar'
+            ],
+            [
+                'id' => 10,
+                'name' => 'Arif Khoirudin',
+                'email' => 'akh@gmail.com',
+                'password' => Hash::make('password123!'),
+                'role' => 'Ketua Kamar'
+            ],
+            [
+                'id' => 11,
+                'name' => 'Oky Pratama Saputra',
+                'email' => 'ops@gmail.com',
+                'password' => Hash::make('password123!'),
+                'role' => 'Ketua Kamar'
+            ],
+            [
+                'id' => 12,
+                'name' => 'Robingu Adi Guna',
+                'email' => 'rag@gmail.com',
+                'password' => Hash::make('password123!'),
+                'role' => 'Ketua Kamar'
+            ],
+            [
+                'id' => 13,
+                'name' => 'Revan Eka Saputra',
+                'email' => 'res@gmail.com',
+                'password' => Hash::make('password123!'),
+                'role' => 'Ketua Kamar'
+            ],
+            [
+                'id' => 14,
+                'name' => 'Aulia Pratama Putra',
+                'email' => 'app@gmail.com',
+                'password' => Hash::make('password123!'),
+                'role' => 'Ketua Kamar'
+            ],
         ])->each(function ($user){
             DB::table('users')->insert($user);
         });
